dfeb28e0890aed4437f73bf86a6d7d5f6e1f4535
Ademikel <adetoromichael346@gmail.com>
2021-03-27 22:28:02 -0700

    Initial commit

---
+++ h.js
@@ -0,0 +1,13 @@
+window.sr=ScrollReveal();
+sr.reveal('.btt', {
+  origin:'right',
+  duration: 3000,
+  distance: '50px',
+  delay:300,
+});
+sr.reveal('img', {
+  origin:'up',
+  duration: 700,
+  distance: '100px',
+  delay:500,
+});
---
+++ ictacad.js
@@ -0,0 +1,23 @@
+var navbar = document.querySelector(".navbar");
+window.onscroll = function() {scroll()};
+function scroll() {
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky");
+    } 
+    else {
+        navbar.classList.remove("sticky"); 
+    }
+}
+window.sr=ScrollReveal();
+sr.reveal('.title', {
+  origin:'top',
+  duration: 2000,
+  distance: '50px',
+  delay:300,
+});
+sr.reveal('.mybtn', {
+  origin:'bottom',
+  duration: 1000,
+  distance: '100px',
+  delay:500,
+});
