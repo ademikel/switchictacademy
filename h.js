c25f482a0debd466a062df198222a4d42fa84e93
Ademikel <adetoromicahel346@gmail.com>
2021-03-29 07:04:43 -0700

    new files added

--- h.js
+++ h.js
@@ -5,3 +5,8 @@ sr.reveal('.btt', {
   distance: '50px',
   delay:300,
 });
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 100,
+  wrap: false
+})
