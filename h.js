ce37def3d810cc5d97b062deb4ab92faaa73ac4c
Ademikel <adetoromicahel346@gmail.com>
2021-03-28 19:19:47 -0700

    new

--- h.js
+++ h.js
@@ -1,13 +1,7 @@
 window.sr=ScrollReveal();
 sr.reveal('.btt', {
   origin:'right',
-  duration: 3000,
+  duration: 1000,
   distance: '50px',
   delay:300,
-});
-sr.reveal('img', {
-  origin:'up',
-  duration: 700,
-  distance: '100px',
-  delay:500,
 });
